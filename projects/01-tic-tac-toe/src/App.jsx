6c770d3cf70008462d9ff7afe67bc5298ff5522d
Antonio Maroto Blasco <antoniomarotoblasco@gmail.com>
2024-03-14 12:21:51 +0100

    Añadida funcionalidad del localStorage

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,21 +1,38 @@
 // import './App.css'
 
-import { WinnerModal } from "./components/WinnerModal.jsx";
+import {
+  resetGameFromStorage,
+  saveGameToStorage,
+} from "./logic/storage/index.js";
 import { checkWinnerFrom, checkEndGame } from "./logic/board.js";
+import { WinnerModal } from "./components/WinnerModal.jsx";
 import { Square } from "./components/Square.jsx";
 import confetti from "canvas-confetti";
 import { TURNS } from "./constants.js";
 import { useState } from "react";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  // LOS HOOKS (useState) NUNCA DEBEN ESTAR DENTRO DE IFs
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
+  });
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem("turn");
+    // ?? Se mira si es null o undefined
+    return turnFromStorage ?? TURNS.X;
+  });
+
   const [winner, setWinner] = useState(null); // null no hay ganador y false hay empate
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    resetGameFromStorage();
   };
 
   const updateBoard = (index) => {
@@ -31,6 +48,12 @@ function App() {
     // Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    // Guardar aquí la partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn,
+    });
 
     // Revisamos si hay un ganador
     const newWinner = checkWinnerFrom(newBoard);
