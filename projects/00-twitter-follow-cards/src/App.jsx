d812cec8d8bc2dfddef770aea4e9d5f267bb8461
Antonio Maroto <a.maroto@innovation.es>
2024-04-23 11:48:26 +0200

    Tarjetas de twitter acabada

--- projects/00-twitter-follow-cards/src/App.jsx
+++ projects/00-twitter-follow-cards/src/App.jsx
@@ -2,12 +2,28 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
+  const antonioMaroto = { isFollowing: true, userName: "AMaroto98" };
 
-  const antonioMaroto = { isFollowing: true, userName: 'AMaroto98'}
-  
+  const users = [
+    {
+      userName: "AMaroto98",
+      name: "Antonio Maroto",
+      isFollowing: true,
+    },
+    {
+      userName: "ElonMusk",
+      name: "Elon Musk",
+      isFollowing: false,
+    },
+    {
+      userName: "CharlieHB",
+      name: "Carlos",
+      isFollowing: true,
+    },
+  ];
+
   return (
     <section className="App">
-
       {/* 
         Para poner el contenido del name entre los tag hay usar el children
         Lo que se pone enmedio de los tags son childrens y para recuperar la informacion hay que modificar el componente padre
@@ -22,17 +38,27 @@ export function App() {
 
       {/* Se pueden pasar todas las props juntas, en lugar de por separado, se necesita un objeto como el que tenemos arriba */}
       {/* Esta considerado mala practica */}
-      <TwitterFollowCard {...antonioMaroto}>
-        Antonio Maroto
-      </TwitterFollowCard>
+      <TwitterFollowCard {...antonioMaroto}>Antonio Maroto</TwitterFollowCard>
 
       {/* Asi estaba al principio pasando todas las props */}
-      {/* <TwitterFollowCard
+      <TwitterFollowCard
         userName={"elonmusk"}
         name={"Elon Musk"}
         isFollowing={false}
-      /> */}
+      />
 
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            isFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        );
+      })}
     </section>
   );
 }
--- projects/00-twitter-follow-cards/src/TwitterFollowCard.jsx
+++ projects/00-twitter-follow-cards/src/TwitterFollowCard.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
-export function TwitterFollowCard({userName = 'unkoown', children, initialisFollowing}) {
+// eslint-disable-next-line react/prop-types
+export function TwitterFollowCard({userName = 'W', children, initialisFollowing}) {
 
     // useState nos permitira guardar en una variable el estado para saber si estamos o no siguiendo a un usuario
     const [isFollowing, setIsFollowing] = useState(initialisFollowing)
